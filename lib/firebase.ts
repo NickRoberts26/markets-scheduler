cde9982d9cc0466aaddff00b610c25af7f719d61
NickRoberts26 <n26roberts@gmail.com>
2024-11-05 12:24:33 +1100

    env.local

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDzwcHKZGF_YhzoIsif0u_yXcjTEJOp0Zs",
-    authDomain: "marketeer-75b47.firebaseapp.com",
-    projectId: "marketeer-75b47",
-    storageBucket: "marketeer-75b47.firebasestorage.app",
-    messagingSenderId: "937089964935",
-    appId: "1:937089964935:web:bf76d4bbea28de914d5122"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
