bad9375127cb9914061ba1f0e4eb48c8aa2c94c7
NickRoberts26 <n26roberts@gmail.com>
2024-11-20 17:58:55 +1100

    Trying to work out how dynamic routing works

--- app/local-markets/page.tsx
+++ app/local-markets/page.tsx
@@ -2,48 +2,15 @@ import React from 'react'
 import Image from "next/image";
 import Link from 'next/link';
 import Sidebar from '@/components/Sidebar';
+import MarketsFeed from '@/components/MarketsFeed';
 
 const LocalMarkets = () => {
   return (
     <div className='flex h-screen max-h-screen'>
         <Sidebar />
-      <div className='px-10 py-8'>
+      <div className='px-10 py-8 w-full'>
         <h1 className='text-5xl font-bold mb-8'>Current Markets</h1>
-        <div className='grid grid-cols-3 gap-x-5'>
-            <Link href="" className='relative h-fit transition-all duration-500 hover:scale-105'>
-                <div className='absolute bg-gray-600 opacity-50 top-0 left-0 h-full w-full rounded-3xl border-4 border-green-500'></div>
-                <div className='absolute top-0 left-0 h-full w-full text-white flex items-center justify-center text-3xl'>Kirribili Markets</div>
-                <Image
-                    src='/assets/markets/kirribilli-markets.jpg'
-                    alt="markets"
-                    width={1000}
-                    height={1000}
-                    className='border-4 border-green-500 rounded-3xl object-cover h-[40vh]'
-                />
-            </Link>
-            <Link href="" className='relative h-fit transition-all duration-500 hover:scale-105'>
-                <div className='absolute bg-gray-600 opacity-50 top-0 left-0 h-full w-full rounded-3xl border-4 border-green-500'></div>
-                <div className='absolute top-0 left-0 h-full w-full text-white flex items-center justify-center text-3xl'>Finders Keepers</div>
-                <Image
-                    src='/assets/markets/finders-keepers.jpg'
-                    alt="markets"
-                    width={1000}
-                    height={1000}
-                    className='border-4 border-green-500 rounded-3xl object-cover h-[40vh]'
-                />
-            </Link>
-            <Link href="" className='relative h-fit transition-all duration-500 hover:scale-105'>
-                <div className='absolute bg-gray-600 opacity-50 top-0 left-0 h-full w-full rounded-3xl border-4 border-green-500'></div>
-                <div className='absolute top-0 left-0 h-full w-full text-white flex items-center justify-center text-3xl'>Bondi Markets</div>
-                <Image
-                    src='/assets/markets/bondi-markets.png'
-                    alt="markets"
-                    width={1000}
-                    height={1000}
-                    className='border-4 border-green-500 rounded-3xl object-cover h-[40vh]'
-                />
-            </Link>
-        </div>
+        <MarketsFeed />
       </div>
     </div>
   )
---
+++ components/MarketsFeed.tsx
@@ -0,0 +1,61 @@
+'use client'
+
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import Image from "next/image";
+import { collection, getDocs, query } from 'firebase/firestore';
+import { db } from '@/lib/firebase';
+
+interface Marketplace {
+    marketplaceName: string;
+    type: string;
+    email: string;
+    contactNumber: string;
+    capacity: string;
+    uid: string;
+}
+
+const MarketsFeed = () => {
+    const [marketplaces, setMarketplaces] = useState<Marketplace[]>();
+
+    useEffect(() => {
+        const fetchMarketplaces = async () => {
+            try {
+                const marketplaceRef = collection(db, 'marketplaces');
+                const q = query(marketplaceRef);
+                const querySnapshot = await getDocs(q);
+
+                const data = querySnapshot.docs.map((doc) => ({
+                    marketplaceName: doc.data().marketplaceName as string,
+                    type: doc.data().type as string,
+                    email: doc.data().email as string,
+                    contactNumber: doc.data().contactNumber as string,
+                    capacity: doc.data().capacity as string,
+                    uid: doc.data().uid as string,
+                }));
+
+                setMarketplaces(data);
+            } catch (error) {
+                
+            }
+        }
+
+        fetchMarketplaces();
+    }, [])
+
+    return (
+        <div className='grid grid-cols-3 gap-x-5'>
+            {marketplaces?.map((marketplace, _) => {
+                console.log(marketplace);
+                return (
+                    <Link key={marketplace.uid} href="" className='relative h-[40vh] mb-5 transition-all duration-500 hover:scale-105'>
+                        <div className='absolute bg-gray-600 opacity-50 top-0 left-0 h-full w-full rounded-3xl border-4 border-green-500'></div>
+                        <div className='absolute top-0 left-0 h-full w-full text-white flex items-center justify-center text-3xl'>{marketplace.marketplaceName}</div>
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
+
+export default MarketsFeed
