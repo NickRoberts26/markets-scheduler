df4e30395af7de156020d48e3bba9adfec3b3532
NickRoberts26 <n26roberts@gmail.com>
2024-12-05 15:27:54 +1100

    Pruned and removed non error console logs

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,11 @@ const nextConfig: NextConfig = {
   trailingSlash: true
 };
 
+module.exports = {
+  compiler: {
+    // Remove all console logs, excluding error logs
+    removeConsole: { exclude: ["error"] }
+  }
+};
+
 export default nextConfig;
