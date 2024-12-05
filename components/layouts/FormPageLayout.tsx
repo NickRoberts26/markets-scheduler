33239dd8f456d1cfc7264329bb691a487b754e2b
NickRoberts26 <n26roberts@gmail.com>
2024-12-05 17:02:37 +1100

    Changed image path to hopefully fix

--- components/layouts/FormPageLayout.tsx
+++ components/layouts/FormPageLayout.tsx
@@ -48,7 +48,7 @@ const FormPageLayout: React.FC<FormPageLayoutProps> = ({ children }) => {
                 </div>
             </div>
             <Image
-                src="/assets/markets-cropped.png"
+                src="/assets/markets-cropped.PNG"
                 alt="markets"
                 width={1000}
                 height={1000}
